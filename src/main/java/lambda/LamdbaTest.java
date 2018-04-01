f971a7ee3db9369bc2c370629d379dc3a046c66e
jikai.sun <425359983@qq.com>
2018-04-01 21:48:14 +0800

    Lamdba 修改 刚才那种写法 是不符合规范的！

--- src/main/java/lambda/LamdbaTest.java
+++ src/main/java/lambda/LamdbaTest.java
@@ -119,4 +119,12 @@ public class LamdbaTest {
         System.out.print(tn);
     }
 
+    //计算流中所有数之和
+    @Test
+    public void addUP(){
+        List<Integer> wordList = Arrays.asList(1, 2, 3);
+        Optional<Integer> reduce = wordList.stream().reduce((nu1, nu2) -> nu1 + nu2);
+        System.out.print(reduce.get());
+    }
+
 }
