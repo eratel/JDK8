b44ed6d4b21aef5154ef6456f87c918fa57643b6
jikai.sun <425359983@qq.com>
2018-04-05 10:57:20 +0800

    Lamdba ThreadLocal  线程并发

--- src/main/java/lambda/UserArgsInterface.java
+++ src/main/java/lambda/UserArgsInterface.java
@@ -1,11 +1,12 @@
 package lambda;
 
-//@FunctionalInterface标记在接口上，“函数式接口”是指仅仅只包含一个抽象方法的接口
+import java.util.function.BinaryOperator;
+
+//标记在接口上，函数式接口 是指仅仅只包含一个抽象方法的接口
 @FunctionalInterface
 public interface UserArgsInterface {
     void study(User user);
-
-    public default void welcome(){
+    default void welcome(){
         System.out.print("welcome+++++++++++++++++++");
     }
 }
